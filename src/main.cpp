65c75ccda42c31f292b169769793398cb5416b72
Sumanth <sumugowd@gmail.com>
2026-03-14 16:21:27 +0530

    Add main.cpp and project structure

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Library Management System ===" << endl;
+    cout << "System Initialized Successfully!" << endl;
+
+    return 0;
+}
